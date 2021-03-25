5faa91bf09f22f08febef7673735e85eaced68c1
Boltie <Boltie@users.noreply.github.com>
2021-03-25 23:13:02 +0000

    Update passgen_example.py

--- passgen_example.py
+++ passgen_example.py
@@ -9,21 +9,29 @@ pg = PassGen(count=2, length=18, sets_enabled="110")
 
 # 1
 pw = pg.get_single()
-print("1 - " + pw) # 1 password printed.
+print("1 - " + pw) # 1 18-digit password printed to console/terminal.
 
 # 2
-pw = pg.get_multiple() # Count of 2 was set upon initialisation.
-for s in pw: print("2 - " + s) # 2 passwords printed.
+pw = pg.get_multiple() # Count of 2 was set at initialisation.
+for s in pw: print("2 - " + s) # 2 18-digit passwords printed to console/terminal.
 
 # 3
 pw = pg.get_multiple(5) # Count of 5 provided - the PassGen object count (pg.count) is also updated to 5.
-for s in pw: print("3 - " + s) # 5 passwords printed.
+for s in pw: print("3 - " + s) # 5 18-digit passwords printed to console/terminal.
 
 # 4
-pw = pg.get_multiple() # get_multiple() defaults to the PassGen object count unless a new count is provided.
-for s in pw: print("4 - " + s) # 5 passwords printed.
+pw = pg.get_multiple() # 'get_multiple()' defaults to the PassGen object count unless a new count is provided.
+for s in pw: print("4 - " + s) # 5 18-digit passwords printed to console/terminal.
 
 # 5
 pg.count = 2 # PassGen object count updated to 2.
 pw = pg.get_multiple()
-for s in pw: print("5 - " + s) # 2 passwords printed.
+for s in pw: print("5 - " + s) # 2 18-digit passwords printed to console/terminal.
+
+# 6
+pw = pg.get_multiple(length=8, sets_enabled="00") # The provided 'sets_enabled' will disable the lower and upper sets.
+for s in pw: print("6 - " + s) # 2 8-digit passwords printed to console/terminal. Only numbers and punctuation used.
+
+#7
+pw = pg.get_single(24) # Generate password providing a length of 24.
+print("7 - " + pw) # 1 24-digit password printed to console/terminal. Only numbers and punctuation used.
